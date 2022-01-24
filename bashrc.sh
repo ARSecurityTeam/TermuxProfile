ddb990acfabcb1295b5707c344413a3e425273f1
Romeo <67787024+ARSecurityTeam@users.noreply.github.com>
2022-01-24 21:07:49 +0600

    Create bashrc.sh

---
+++ bashrc.sh
@@ -0,0 +1,84 @@
+command_not_found_handle() {
+	/data/data/com.termux/files/usr/libexec/termux/command-not-found "$1"
+}
+
+PS1='\033[1;91mroot@kali[\033[1;92m\w\033[1;91m]#:\033[1;37m'
+clear
+#Code by ARSecurityTeam
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo -e "\e[32m"
+echo -e "
+                  (H)         (R)
+                   \\          /
+                    +hydNNNNdyh+
+                  +mMMMMMMMMMMMMm+
+                 dMM\e[31m(•)\e[32mNMMMNN\e[31m(•)\e[32mMMd
+                hMMMMMMMMMMMMMMMMMMh
+              ..yyyyyyyyyyyyyyyyyyyy..  
+          .mMMm MMMMMMMMMMMMMMMMMMMM mMMm.
+          :MMMM-MMMMMMMMMMMMMMMMMMMM-MMMM:
+          :MMMM-MMMMMMMMMMMMMMMMMMMM-MMMM:
+          :MMMM-MMMMMMMMMMMMMMMMMMMM-MMMM:
+          :MMMM-MMMMMMMMMMMMMMMMMMMM-MMMM:
+          -MMMM-MMMMMMMMMMMMMMMMMMMM-MMMM-
+           +yy+ MMMMMMMMMMMMMMMMMMMM +yy+
+                mMMMMMMMMMMMMMMMMMMm
+                /++MMMMh++hMMMM++/
+                   MMMMo  oMMMM
+                   MMMMo  oMMMM
+                   oNMm    mMNs"
+echo ""
+echo -e "  \e[36m[\e[31m+\e[36m]\e[32m Father \e[34mOf \e[31mHacker.. "
+echo -e "  \e[36m[\e[31m+\e[36m]\e[37m Welcome To \e[32mThe New World \e[31mOf Piracy"
+echo ""
+echo ""
+sleep 3.0
+clear
+echo ""
+echo ""
+echo ""
+echo -e "\e[31m"
+echo -e "   ╔═════════════════════════════════════════════╗"
+echo -e "   \e[31m║   ███████\e[32m╗\e[31m██████\e[32m╗ \e[31m██████\e[32m╗  \e[31m██████\e[32m╗ \e[31m██████\e[32m╗  \e[31m║"
+echo -e "   \e[31m║   ██\e[32m╔════╝\e[31m██\e[32m╔══\e[31m██\e[32m╗\e[31m██\e[32m╔══\e[31m██\e[32m╗\e[31m██\e[32m╔═══\e[31m██\e[32m╗\e[31m██\e[32m╔══\e[31m██\e[32m╗ \e[31m║"
+echo -e "   \e[31m║   █████\e[36m╗  \e[31m██████\e[36m╔╝\e[31m██████\e[36m╔╝\e[31m██\e[36m║   \e[31m██\e[36m║\e[31m██████\e[36m╔╝ \e[31m║"
+echo -e "   \e[31m║   ██\e[36m╔══╝  \e[31m██\e[36m╔══\e[31m██\e[36m╗\e[31m██\e[36m╔══\e[31m██\e[36m╗\e[31m██\e[36m║   \e[31m██\e[36m║\e[31m██\e[36m╔══\e[31m██\e[36m╗ \e[31m║"
+echo -e "   \e[31m║   ███████\e[33m╗\e[31m██\e[33m║  \e[31m██\e[33m║\e[31m██\e[33m║  \e[31m██\e[33m║╚\e[31m██████\e[33m╔╝\e[31m██\e[33m║  \e[31m██\e[33m║ \e[31m║"
+echo -e "   \e[31m║   \e[34m╚══════╝╚═╝  ╚═╝╚═╝  ╚═╝ ╚═════╝ ╚═╝  ╚═╝ \e[31m║"     
+echo -e "   \e[31m║                                             \e[31m║" 
+echo -e "   \e[31m║                                             \e[31m║ "
+echo -e "   \e[31m║            \e[34m████████\e[32m╗\e[34m██\e[32m╗\e[34m███\e[32m╗   \e[34m███\e[32m╗          \e[31m║"
+echo -e "   \e[31m║            \e[32m╚══\e[34m██\e[32m╔══╝\e[34m██\e[32m║\e[34m████\e[32m╗ \e[34m████\e[32m║          \e[31m║"
+echo -e "   \e[31m║               \e[34m██\e[31m║   \e[34m██\e[31m║\e[34m██\e[31m║╚\e[34m██\e[31m╔╝\e[34m██\e[31m║          \e[31m║"
+echo -e "   \e[31m║               \e[34m██\e[31m║   \e[34m██\e[31m║\e[34m██\e[31m║ ╚═╝ \e[34m██\e[31m║          \e[31m║"
+echo -e "   \e[31m║               \e[33m╚═╝   ╚═╝╚═╝     ╚═╝          \e[31m║"
+echo -e "   \e[31m║ \e[36m[\e[31m+\e[36m]\e[37m The \e[33mCyber \e[31mSecurity Hackers..            \e[31m║"
+echo -e "   \e[31m║ \e[36m[\e[31m+\e[36m]\e[32m The \e[35mBlack Hat Hackers..                 \e[31m║"
+echo -e "   \e[31m║ \e[36m[\e[31m+\e[36m]\e[32m Father \e[34mOf \e[31mHacker..                      \e[31m║"
+echo -e "   \e[31m╚═════════════════════════════════════════════╝"
+echo ""
+echo ""
+echo -e "\e[96m"
+date
+echo -e "\e[1;31m"
