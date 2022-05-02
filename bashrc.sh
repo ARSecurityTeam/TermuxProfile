cfd9111544a23d554eb775c6b665f5c49ecfaa9d
Romeo <67787024+ARSecurityTeam@users.noreply.github.com>
2022-05-02 17:17:32 +0600

    bash.bashrc
    update Termux scripte

--- bashrc.sh
+++ bashrc.sh
@@ -1,8 +1,19 @@
-command_not_found_handle() {
-	/data/data/com.termux/files/usr/libexec/termux/command-not-found "$1"
-}
+# Command history tweaks:
+# - Append history instead of overwriting
+#   when shell exits.
+# - When using history substitution, do not
+#   exec command immediately.
+# - Do not save to history commands starting
+#   with space.
+# - Do not save duplicated commands.
+shopt -s histappend
+shopt -s histverify
+export HISTCONTROL=ignoreboth
 
-PS1='\033[1;91mroot@kali[\033[1;92m\w\033[1;91m]#:\033[1;37m'
+# Default command line prompt.
+PROMPT_DIRTRIM=2
+PS1='\[\e[0;32m\]\w\[\e[0m\] \[\e[0;97m\]\$\[\e[0m\] '
+
 clear
 #Code by ARSecurityTeam
 echo ""
@@ -82,3 +93,12 @@ echo ""
 echo -e "\e[96m"
 date
 echo -e "\e[1;31m"
+
+# Handles nonexistent commands.
+# If user has entered command which invokes non-available
+# utility, command-not-found will give a package suggestions.
+if [ -x /data/data/com.termux/files/usr/libexec/termux/command-not-found ]; then
+	command_not_found_handle() {
+		/data/data/com.termux/files/usr/libexec/termux/command-not-found "$1"
+	}
+fi
